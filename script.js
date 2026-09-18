3965c604982da1f235a6e6a380c395ab43188aa0
swatv3nub <maskedvirus@protonmail.com>
2026-09-18 10:48:37 +0530

    placeholder landing page for lab.sw4nit.dev

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener('click', (event) => {
+    const target = document.querySelector(link.getAttribute('href'));
+    if (target) {
+      event.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
